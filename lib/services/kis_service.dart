42bab3aa265c56448ac89f5b20c8d55bc9b86352
bermont <eklim4254@gmail.com>
2025-07-01 14:11:40 +0900

    국내주식 결과 UI 카드 형태 개선

--- lib/services/kis_service.dart
+++ lib/services/kis_service.dart
@@ -2,45 +2,22 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class KisService {
-  final String _proxyUrl =
-      'https://stockwiki-gw35etj6o-bermonts-projects.vercel.app/kis-stock-info';
+  final String _baseUrl = 'https://stockwiki.vercel.app/api/kis-stock-info';
 
-  Future<String> fetchStockInfo(String keyword) async {
-    final isCode = RegExp(r'^\d{5,6}$').hasMatch(keyword);
-    String code = keyword;
-
-    if (!isCode) {
-      final searchUri = Uri.parse('$_proxyUrl?pdno=$code');
-      final searchRes = await http.get(searchUri);
-
-      if (searchRes.statusCode == 200) {
-        final result = json.decode(searchRes.body);
-        if (result.isNotEmpty) {
-          code = result[0]['pdno']; // 종목코드 추출
-        } else {
-          return '종목 검색 결과 없음';
-        }
-      } else {
-        throw Exception('KIS 종목 검색 API 호출 실패');
-      }
-    }
-
-    final uri = Uri.parse('$_proxyUrl?code=$code');
+  Future<Map<String, dynamic>> fetchStockInfo(String code) async {
+    final uri = Uri.parse('$_baseUrl?code=$code');
     final response = await http.get(uri);
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final output = data['output'];
-
-      if (output == null || output is! Map) return '검색 결과 없음';
-
-      final name = output['prdt_abrv_name'] ?? '이름없음';
-      final pdno = output['pdno'] ?? '코드없음';
-      final price = output['stck_prpr'] ?? 'N/A';
-
-      return '$name ($pdno) - ₩$price';
+      if (output is Map<String, dynamic>) {
+        return output;
+      } else {
+        throw Exception('output 파싱 실패');
+      }
     } else {
-      throw Exception('KIS 상세정보 API 호출 실패: ${response.statusCode}');
+      throw Exception('KIS API 실패 (${response.statusCode})');
     }
   }
 }
