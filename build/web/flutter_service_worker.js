c8e637f2e09a672ef02f8e9a17cdfe6bc8ed83f4
bermont <eklim4254@gmail.com>
2025-07-02 17:50:07 +0900

    null

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "81032f68f736fe760430a846c323407d
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "298270c7dc0194fbcb8087167bcf9eb2",
+"flutter_bootstrap.js": "2196c558eedf3e0dc94f3560087b7a2c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
