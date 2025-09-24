1417e6ad3f8fd415488cdda530dbbeabd1b8b98b
bermont <eklim4254@gmail.com>
2025-09-24 15:06:24 +0900

    차트를 일봉 캔들 차트만 표시하도록 단순화

--- api/stock-chart.js
+++ api/stock-chart.js
@@ -40,42 +40,11 @@ module.exports = async function handler(req, res) {
 }
 
 function generateChartUrls(symbol) {
-  // 네이버 증권 차트 이미지 URL들 (실제 작동하는 URL 구조)
+  // 네이버 증권 일봉 캔들 차트만 제공
   const baseUrl = 'https://ssl.pstatic.net/imgfinance/chart';
   
   return {
-    // 일봉 차트 (area)
-    daily: `${baseUrl}/item/area/day/${symbol}.png`,
-    
-    // 주봉 차트 (area)
-    weekly: `${baseUrl}/item/area/week/${symbol}.png`,
-    
-    // 월봉 차트 (area)
-    monthly: `${baseUrl}/item/area/month/${symbol}.png`,
-    
-    // 일봉 차트 (candle)
-    candle: `${baseUrl}/item/candle/day/${symbol}.png`,
-    
-    // 주봉 차트 (candle)
-    candle_weekly: `${baseUrl}/item/candle/week/${symbol}.png`,
-    
-    // 월봉 차트 (candle)
-    candle_monthly: `${baseUrl}/item/candle/month/${symbol}.png`,
-    
-    // 거래량 차트
-    volume: `${baseUrl}/item/volume/day/${symbol}.png`,
-    
-    // 분봉 차트 (실제 작동하는 URL)
-    minute5: `${baseUrl}/item/area/minute5/${symbol}.png`,
-    minute15: `${baseUrl}/item/area/minute15/${symbol}.png`,
-    minute30: `${baseUrl}/item/area/minute30/${symbol}.png`,
-    minute60: `${baseUrl}/item/area/minute60/${symbol}.png`,
-    
-    // 기술적 지표 (실제 작동하는 URL)
-    technical: `${baseUrl}/item/technical/day/${symbol}.png`,
-    
-    // 추가 차트 타입들
-    line: `${baseUrl}/item/line/day/${symbol}.png`,
-    bar: `${baseUrl}/item/bar/day/${symbol}.png`
+    // 일봉 캔들 차트만
+    candle: `${baseUrl}/item/candle/day/${symbol}.png`
   };
 }
