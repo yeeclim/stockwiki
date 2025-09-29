26b7501a202adb6a3c7de9900b4c367b2829f107
bermont <eklim4254@gmail.com>
2025-09-29 17:38:34 +0900

    검색결과 레이아웃 수정

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2445406770"
+    serviceWorkerVersion: "1741185454"
   }
 });
