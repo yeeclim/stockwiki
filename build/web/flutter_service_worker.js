e03b7d017b624549609d48e8dbc0b48b79e5a4d7
bermont <eklim4254@gmail.com>
2025-09-30 16:27:03 +0900

    Ai추천 기능 정리

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9148b3735c5dbfed28abc83e8ae1632d
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "4bb3b6fcf4305e8e277c8e062334a5ad",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "43b65ac6eff56211c06e1922b3796600",
+"flutter_bootstrap.js": "4ff060b91c93a80f8784404eaf17644f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
