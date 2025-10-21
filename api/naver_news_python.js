79cb26d5266a1ebc63cf1b197b25df6c51d9ffa3
bermont <eklim4254@gmail.com>
2025-10-21 14:01:22 +0900

    re

--- api/naver_news_python.js
+++ api/naver_news_python.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Content-Type', 'application/json');
 
   // OPTIONS 요청 처리
   if (req.method === 'OPTIONS') {
