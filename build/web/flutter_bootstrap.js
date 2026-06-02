576f2d0cdd3d55a3143531c02f561dac59054286
bermont <eklim4254@gmail.com>
2026-06-02 15:59:16 +0900

    feat: 테마별 추천 종목에 스크리닝 섹터 탭 연동 (Supabase 공유)

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3680589738" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1923002678" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
